9138d6ead44381cd6113b324db034c96dda69b5c
unknown <prabhjotarora99@gmail.com>
2024-03-29 15:27:37 +0530

    Major change

--- chatbot/views.py
+++ chatbot/views.py
@@ -9,7 +9,10 @@ def chat(request):
         if request.method == 'POST':
          message = request.POST.get('message')
          print(message)
-         return JsonResponse({'message':'Hello...This is a simple message. The model is in a development phase. Every Time it will generate the same response for your query. Please wait for the full development of this Chat-Bot. Thanks...'})
+         data = [
+         {'message': 'Hello...This is a simple message. The model is in a development phase. Every Time it will generate the same response for your query. Please wait for the full development of this Chat-Bot.',
+         'url': 'https://github.com/prabhjot-arora31/D_C/'}]
+         return JsonResponse(data,safe=False)
         elif request.method == 'GET':
          return render(request,'home.html')
         else:
