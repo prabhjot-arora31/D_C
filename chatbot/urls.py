8ca1d06f4199bbb190e6b97a68613ac2908b8112
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-03-29 15:59:45 +0530

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.chat),
-    path('contact/', views.contact),
+    path('/contact', views.contact),
     # path('chat/', views.chat_message),  # Remove the leading slash
 ]
